606f896f6722f4fdcf40a3806e164d7e74a112f4
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-09-04 00:25:33 +0300

    Add Skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,7 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: PlatForm }[];
-  metacritic:number; 
+  metacritic: number;
 }
 
 interface fetchGamRes {
@@ -22,21 +22,27 @@ interface fetchGamRes {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setErrors] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<fetchGamRes>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setErrors(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
