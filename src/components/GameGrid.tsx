39d1a532426b6fc05ee713211b38e6f33f160ff9
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-09-04 00:08:34 +0300

    get optimized images

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ const GameGrid = () => {
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
                 {games.map((game) => (<GameCard key={game.id} game={game} />))}
             </SimpleGrid>
         </>
