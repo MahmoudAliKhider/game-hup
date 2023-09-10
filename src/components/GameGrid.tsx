bf38de9bb7a560e057ffa9ca5b04c7db28b30ca2
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-09-10 22:04:59 +0300

    updates

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,5 @@
 import { SimpleGrid, Text } from '@chakra-ui/react'
-import useGames, { Game } from "../hooks/useGames";
+import { Game } from "../hooks/useGames";
 import GameCard from './GameCard';
 import GameCardSceleton from './GameCardSceleton';
 import GameCardContontainer from './gameCardContontainer';
