899f9e1ec398329d1f2a47a93548899ba2ba5960
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-09-12 02:33:45 +0300

    Add Game Heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Genre } from './hooks/useGenres'
 import { PlatForm } from './hooks/useGames'
 import PlatformSelector from './components/PlatformSelector'
 import SortSelector from './components/SortSelector'
+import GameHeading from './components/GameHeading'
 
 export interface GameQuery {
   genre: Genre | null;
@@ -29,6 +30,7 @@ function App() {
       </GridItem>
     </Show>
     <GridItem area='main'>
+      <GameHeading  gameQuery={gameQuery} />
       <PlatformSelector selectedPlatform={gameQuery.platform} onSelectedPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
       <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
       <GameGrid gameQuery={gameQuery} />
