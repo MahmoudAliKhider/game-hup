ef2bb866a4bf18d436b182aad7e4fe93cf47a158
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-09-12 02:40:18 +0300

    update style GenreList

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, List, ListItem, Image, Text, Spinner, Button } from '@chakra-ui/react';
+import { HStack, List, ListItem, Image, Text, Spinner, Button, Heading } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres'
 import getCroppedImagUrl from '../services/images-url';
 
@@ -11,15 +11,18 @@ const GenreList = ({onSelectedGerne}:Props) => {
     if (isLoading) return <Spinner />;
 
     return (
+        <>
+        <Heading fontSize='3xl' marginBottom={3}>Genres</Heading>
         <List>
             {data.map(gener =>
                 <ListItem key={gener.id} paddingY='6px'>
                     <HStack>
-                        <Image boxSize='47px' borderRadius={8} src={getCroppedImagUrl(gener.image_background)} />
-                        <Button onClick={() => onSelectedGerne(gener)} fontSize='xl' variant='link'>{gener.name}</Button>
+                        <Image boxSize='47px' borderRadius={8} objectFit='cover' src={getCroppedImagUrl(gener.image_background)} />
+                        <Button whiteSpace='normal' textAlign='left' onClick={() => onSelectedGerne(gener)} fontSize='xl' variant='link'>{gener.name}</Button>
                     </HStack>
                 </ListItem>)}
         </List>
+        </>
     )
 }
 
